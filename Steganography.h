e62fa3ea4cbf9fd38a2ef5d7112f3bc269b361ff
Matthew Deck <matedeck@ut.utm.edu>
2023-09-25 09:30:59 -0500

    added cipher logic

---
+++ Steganography.h
@@ -0,0 +1,30 @@
+#ifndef STEGANOGRAPHY_H
+#define STEGANOGRAPHY_H
+
+#include <string>
+#include <vector>
+
+class Steganography {
+
+    private:
+        std::string magicNumber;
+        int width;
+        int height;
+        int maxColor;
+        std::vector<int> colorData;
+        std::string cipherText;
+        int getNthBit(char cipherChar, int n);
+
+    public:
+        Steganography();
+        void readImage(std::string fileName);
+        void printImage(std::string fileName);
+        void readCipherText(std::string fileName);
+        void printCipherText(std::string fileName);
+        void cleanImage();
+        void encipher();
+        void decipher();
+
+};
+
+#endif // STEGANOGRAPHY_H
--- steganography.cpp
+++ steganography.cpp
@@ -1,11 +1,11 @@
-#include "steganography.h"
+#include "Steganography.h"
 #include <fstream>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-void Steg::readImage(string fileName)
+void Steganography::readImage(string fileName)
 {
     ifstream jFile;
     jFile.open(fileName);
@@ -19,7 +19,7 @@ void Steg::readImage(string fileName)
     }
 }
 
-void Steg::printImage(string fileName)
+void Steganography::printImage(string fileName)
 {
     ifstream jFile;
     jFile.open(fileName);
@@ -36,8 +36,59 @@ void Steg::printImage(string fileName)
     }
 }
 
-int Steg::getNthBit(char cipherChar, int n)
-{
-    
-    return 
+void Steganography::cleanImage() {
+
+    // replace each least significant bit in colorData with zero
+    for (int i = 0; i < colorData.size(); i++) {
+        colorData[i] &= ~(1);
+    }
+}
+
+int Steganography::getNthBit(char cipherChar, int n) {
+    return (cipherChar >> n) & 1;
+}
+
+void Steganography::encipher() {
+
+    // check there are enough pixels to encipher entire text
+    if (colorData.size() < cipherText.length() * 8) throw runtime_error("PPM image too small to encipher text");
+
+    // index for colorData
+    int index = 0;
+
+    // loop over each character in text
+    for (int pos = 0; pos < cipherText.length(); pos++) {
+
+        // encipher character into least significant character bit across next eight values in colorData
+        for (int bit = 7; bit >=0; bit--) {
+            colorData[index] |= getNthBit(cipherText[pos], bit); // using the required helper function
+            index++;
+        }
+    }
+}
+
+void Steganography::decipher() {
+
+    // initialize output text
+    cipherText = "";
+
+    // loop over each value in colorData 
+    // for() increase intentionally excluded, index incremented inside inner for loop
+    for (int index = 0; index < colorData.size(); ) {
+
+        // create new character value
+        char val = 0;
+
+        // extract least significant bit of next eight colorData values to reconstruct character
+        for (int bit = 7; bit >= 0; bit--) {
+            val |= (colorData[index] & 1) << bit;
+            index++;
+        }
+
+        // if no character value, assume end of text
+        if (val == 0) return;
+
+        // push character value onto end of text string
+        cipherText.push_back(val);
+    }
 }
